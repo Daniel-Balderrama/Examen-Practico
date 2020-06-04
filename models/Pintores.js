e84759f262b341606f95a6c055c17c00b03524c5
Daniel Balderrama <65323724+Daniel-Balderrama@users.noreply.github.com>
2020-06-03 22:37:24 -0600

    Models

---
+++ models/Pintores.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+//crear un Schema
+
+let Schema = mongoose.Schema;
+
+//crear coleccion
+let PintoresSchema = new Schema({
+    nombre:{
+        type: String,
+        requeried: [true, 'Requerimos el nombre']
+    },
+    corriente:{
+        type: String,
+        required: [true, 'Requerimos la corriente']
+    },
+    natalicio:{
+        type: String,
+        required: [true, 'Requerimos fecha de nacimiento']
+    },
+    pintura:{
+        type: String,
+    }
+});
+
+module.exports = mongoose.model('Pintores', PintoresSchema);
